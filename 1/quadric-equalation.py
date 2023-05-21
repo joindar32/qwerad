16d6cbf4e0c7a674032f7e914d00ffcd1b07a153
joindar32 <125015919+joindar32@users.noreply.github.com>
2023-05-21 19:30:55 +0300

    Update quadric-equalation.py

--- 1/quadric-equalation.py
+++ 1/quadric-equalation.py
@@ -1,1 +1,16 @@
+a = int(input("a"))
+b = int(input("b"))
+c = int(input("c"))
+
+d = b**2-4*a*c # discriminant
+
+if d < 0:
+    print ("no real solution")
+elif d == 0:
+    x = (-b+math.sqrt(b**2-4*a*c))/2*a
+    print ("one solution:"), x
+else:
+    x1 = (-b+math.sqrt((b**2)-(4*(a*c))))/(2*a)
+    x2 = (-b-math.sqrt((b**2)-(4*(a*c))))/(2*a)
+    print ("two solutions: ", x1, " or", x2)
 
